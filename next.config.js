7b75ee27af9fc4267053daad9e98373a7293e86c
OPunga <ronnyopunga96@gmail.com>
2026-08-07 16:16:37 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isProjectPages = Boolean(repositoryName) && !repositoryName.endsWith('.github.io');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: isProjectPages ? `/${repositoryName}` : '',
+  assetPrefix: isProjectPages ? `/${repositoryName}/` : '',
   images: {
     unoptimized: true,
     remotePatterns: [
