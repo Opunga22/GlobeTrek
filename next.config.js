3cdde6409eb2df02ae3a954fd239562baa1c37c9
OPunga <ronnyopunga96@gmail.com>
2026-08-07 23:43:07 +0300

    Fix Next.js GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
 
@@ -6,13 +7,6 @@ const nextConfig = {
 
   images: {
     unoptimized: true,
-
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
   },
 };
 
